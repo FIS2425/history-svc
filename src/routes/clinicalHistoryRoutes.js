f15d40242aeba97d804153b6508afa3855228420
Daniel Arriaza Arriaza <113770002+darkgigi@users.noreply.github.com>
2024-11-15 19:02:12 +0100

    feat: added treatment routes
    Co-Authored-By: Rafael González Castillero <64226756+rafgoncas1@users.noreply.github.com>

--- src/routes/clinicalHistoryRoutes.js
+++ src/routes/clinicalHistoryRoutes.js
@@ -1,12 +1,19 @@
 import * as clinicalHistoryController from '../controllers/clinicalHistoryController.js';
+import * as treatmentController from '../controllers/treatmentController.js';
 import express from 'express';
 const router = express.Router();
 
+// Clinical history routes
 router.delete('/patient/:patientId', clinicalHistoryController.deleteClinicalHistoryByPatientId);
 router.post('/', clinicalHistoryController.createClinicalHistory);
 router.get('/', clinicalHistoryController.getAllClinicalHistories);
 router.get('/:id', clinicalHistoryController.getClinicalHistoryById);
 router.get('/patient/:patientId', clinicalHistoryController.getClinicalHistoryByPatientId);
 router.delete('/:id', clinicalHistoryController.deleteClinicalHistoryById);
+
+// Treatment routes
+router.post('/:id/treatment', treatmentController.addTreatment);
+router.delete('/:id/treatment/:treatmentId', treatmentController.deleteTreatment);
+router.put('/:id/treatment/:treatmentId', treatmentController.updateTreatment);
 
 export default router;
