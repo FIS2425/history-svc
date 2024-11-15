0db74f5c3bdf835bcbb3bf647899c46e1cf2f75a
Rafael González <rafaelgonzalezcastillero@gmail.com>
2024-11-15 20:40:58 +0100

    feat: update current condition model
    Co-authored-by: Daniel Arriaza Arriaza <darkgigi@users.noreply.github.com>

--- src/models/CurrentCondition.js
+++ src/models/CurrentCondition.js
@@ -12,7 +12,20 @@ const currentConditionSchema = new Schema({
   since: {
     type: Date,
     required: true,
-    default: Date.now,
+    default: () => {
+      const today = new Date();
+      const utc = new Date(Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()));
+      return utc;
+    },
+    validate: {
+      validator: function (value) {
+        // Verify that since is not in the future
+        const today = new Date();
+        const utc = new Date(Date.UTC(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59));
+        return value <= utc;
+      },
+      message: 'Since must be today or in the past',
+    },
   },
 });
 
