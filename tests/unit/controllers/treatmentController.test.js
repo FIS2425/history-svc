7c873754a3fd229d4fdd771db9dd87fe16ce604d
Rafael González <rafaelgonzalezcastillero@gmail.com>
2024-12-30 12:49:22 +0100

    feat: Implement JWT authentication and authorization middleware

--- tests/unit/controllers/treatmentController.test.js
+++ tests/unit/controllers/treatmentController.test.js
@@ -1,17 +1,23 @@
-import { beforeAll, afterAll, describe, expect, it } from 'vitest';
+import { beforeAll, afterAll, describe, expect, it, vi } from 'vitest';
 import ClinicalHistory from '../../../src/models/ClinicalHistory.js';
 import * as db from '../../setup/database';
 import { request } from '../../setup/setup';
 import { v4 as uuidv4 } from 'uuid';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+const cookie = ['token=authToken&refreshToken=refreshToken'];
 
 beforeAll(async () => {
+  vi.spyOn(jwt, 'verify').mockReturnValue({
+    userId: 'userId',
+    roles: ['admin'],
+  });
   await db.clearDatabase();
-  // Insert mock data if necessary
 });
 
 afterAll(async () => {
-  await db.clearDatabase();
+  vi.resetAllMocks();
 });
 
 describe('CLINICAL HISTORY TREATMENT ENDPOINTS TEST', () => {
@@ -23,7 +29,7 @@ describe('CLINICAL HISTORY TREATMENT ENDPOINTS TEST', () => {
         startDate: new Date(),
         endDate: new Date(),
         instructions: 'Take twice daily'
-      });
+      }).set('Cookie', cookie);
       expect(response.status).toBe(404);
       expect(response.body.message).toBe('Clinical history not found');
     });
@@ -39,7 +45,7 @@ describe('CLINICAL HISTORY TREATMENT ENDPOINTS TEST', () => {
         instructions: 'Take twice daily'
       };
 
-      const response = await request.post(`/histories/${newClinicalHistory._id}/treatment`).send(treatmentData);
+      const response = await request.post(`/histories/${newClinicalHistory._id}/treatment`).send(treatmentData).set('Cookie', cookie);
       expect(response.status).toBe(400);
       expect(response.body.errors['treatments.0.endDate']).toContain('End date must be greater than or equal to start date');
     });
@@ -55,7 +61,7 @@ describe('CLINICAL HISTORY TREATMENT ENDPOINTS TEST', () => {
         instructions: 'Take twice daily'
       };
 
-      const response = await request.post(`/histories/${newClinicalHistory._id}/treatment`).send(treatmentData);
+      const response = await request.post(`/histories/${newClinicalHistory._id}/treatment`).send(treatmentData).set('Cookie', cookie);
       expect(response.status).toBe(200);
       expect(response.body.treatments.length).toBe(1);
       expect(response.body.treatments[0].name).toBe(treatmentData.name);
@@ -65,13 +71,13 @@ describe('CLINICAL HISTORY TREATMENT ENDPOINTS TEST', () => {
   describe('test DELETE /histories/:id/treatment/:treatmentId', () => {
 
     it('should return 400 if treatment ID is not an ObjectID', async () => {
-      const response = await request.delete(`/histories/${uuidv4()}/treatment/${uuidv4()}`);
+      const response = await request.delete(`/histories/${uuidv4()}/treatment/${uuidv4()}`).set('Cookie', cookie);
       expect(response.status).toBe(400);
       expect(response.body.message).toBe('Treatment ID is not valid');
     });
 
     it('should return 404 if clinical history is not found', async () => {
-      const response = await request.delete(`/histories/${uuidv4()}/treatment/${new mongoose.Types.ObjectId()}`);
+      const response = await request.delete(`/histories/${uuidv4()}/treatment/${new mongoose.Types.ObjectId()}`).set('Cookie', cookie);
       expect(response.status).toBe(404);
       expect(response.body.message).toBe('Clinical history not found');
     });
@@ -80,7 +86,7 @@ describe('CLINICAL HISTORY TREATMENT ENDPOINTS TEST', () => {
       const newClinicalHistory = new ClinicalHistory({ patientId: uuidv4() });
       await newClinicalHistory.save();
 
-      const response = await request.delete(`/histories/${newClinicalHistory._id}/treatment/${new mongoose.Types.ObjectId()}`);
+      const response = await request.delete(`/histories/${newClinicalHistory._id}/treatment/${new mongoose.Types.ObjectId()}`).set('Cookie', cookie);
       expect(response.status).toBe(404);
       expect(response.body.message).toBe('Treatment not found');
     });
@@ -93,7 +99,7 @@ describe('CLINICAL HISTORY TREATMENT ENDPOINTS TEST', () => {
       await newClinicalHistory.save();
 
       const treatmentId = newClinicalHistory.treatments[0]._id;
-      const response = await request.delete(`/histories/${newClinicalHistory._id}/treatment/${treatmentId}`);
+      const response = await request.delete(`/histories/${newClinicalHistory._id}/treatment/${treatmentId}`).set('Cookie', cookie);
       expect(response.status).toBe(200);
       expect(response.body.treatments.length).toBe(0);
 
@@ -110,7 +116,7 @@ describe('CLINICAL HISTORY TREATMENT ENDPOINTS TEST', () => {
         startDate: new Date(),
         endDate: new Date(),
         instructions: 'Updated instructions'
-      });
+      }).set('Cookie', cookie);
       expect(response.status).toBe(404);
       expect(response.body.message).toBe('Clinical history not found');
     });
@@ -124,7 +130,7 @@ describe('CLINICAL HISTORY TREATMENT ENDPOINTS TEST', () => {
         startDate: new Date(),
         endDate: new Date(),
         instructions: 'Updated instructions'
-      });
+      }).set('Cookie', cookie);
       expect(response.status).toBe(404);
       expect(response.body.message).toBe('Treatment not found');
     });
@@ -144,7 +150,7 @@ describe('CLINICAL HISTORY TREATMENT ENDPOINTS TEST', () => {
         instructions: 'Updated instructions'
       };
 
-      const response = await request.put(`/histories/${newClinicalHistory._id}/treatment/${treatmentId}`).send(updatedTreatmentData);
+      const response = await request.put(`/histories/${newClinicalHistory._id}/treatment/${treatmentId}`).send(updatedTreatmentData).set('Cookie', cookie);
       expect(response.status).toBe(400);
       expect(response.body.errors['treatments.0.endDate']).toContain('End date must be greater than or equal to start date');
     });
@@ -164,7 +170,7 @@ describe('CLINICAL HISTORY TREATMENT ENDPOINTS TEST', () => {
         instructions: 'Updated instructions'
       };
 
-      const response = await request.put(`/histories/${newClinicalHistory._id}/treatment/${treatmentId}`).send(updatedTreatmentData);
+      const response = await request.put(`/histories/${newClinicalHistory._id}/treatment/${treatmentId}`).send(updatedTreatmentData).set('Cookie', cookie);
       expect(response.status).toBe(200);
       expect(response.body.treatments[0].name).toBe(updatedTreatmentData.name);
     });
---
+++ tests/unit/middleware/verifyTokenAuth.test.js
@@ -0,0 +1,31 @@
+import { describe, expect, it } from 'vitest';
+import { request } from '../../setup/setup';
+import jwt from 'jsonwebtoken';
+
+describe('Authorization middleware', () => {
+  it('should return 401 with no token provided', async () => {
+    const res = await request.get('/histories').set('Cookie', '');
+    expect(res.status).toBe(401);
+    expect(res.body.error).toBe('Access denied: No token provided');
+  });
+  it('should return 401 with invalid token (badly signed)', async () => {
+    const invalidToken = 'invalid.token.value';
+    const res = await request.get('/histories').set('Cookie', `token=${invalidToken}`);
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe('Invalid token');
+  });
+  it('should return 401 with insufficient permissions', async () => {
+    const token = jwt.sign(
+      { userId: '123', roles: ['doctor'] },
+      'secret'
+    );
+    const res = await request.get('/histories').set('Cookie', `token=${token}`);
+    expect(res.status).toBe(403);
+    expect(res.body.error).toBe('Access denied: Insufficient permissions');
+  });
+  it('should validate the token successfully', async () => {
+    const token = jwt.sign( { userId: '123', roles: ['admin'] }, 'secret');
+    const res = await request.get('/histories').set('Cookie', `token=${token}`);
+    expect(res.status).toBe(200);
+  });
+});
