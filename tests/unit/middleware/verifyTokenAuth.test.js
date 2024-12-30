ca59096d245770fba0106f7321bcc074d04d17d7
Daniel Arriaza Arriaza <113770002+darkgigi@users.noreply.github.com>
2024-12-30 12:56:29 +0100

    fix: add secret variables to auth tests

--- tests/unit/middleware/verifyTokenAuth.test.js
+++ tests/unit/middleware/verifyTokenAuth.test.js
@@ -17,14 +17,14 @@ describe('Authorization middleware', () => {
   it('should return 401 with insufficient permissions', async () => {
     const token = jwt.sign(
       { userId: '123', roles: ['doctor'] },
-      'secret'
+      process.env.VITE_JWT_SECRET
     );
     const res = await request.get('/histories').set('Cookie', `token=${token}`);
     expect(res.status).toBe(403);
     expect(res.body.error).toBe('Access denied: Insufficient permissions');
   });
   it('should validate the token successfully', async () => {
-    const token = jwt.sign( { userId: '123', roles: ['admin'] }, 'secret');
+    const token = jwt.sign( { userId: '123', roles: ['admin'] }, process.env.VITE_JWT_SECRET);
     const res = await request.get('/histories').set('Cookie', `token=${token}`);
     expect(res.status).toBe(200);
   });
